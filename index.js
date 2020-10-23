72c69dd3f034e733d6aa7c41fe6017a8768ba4d4
Avishek Adhikari <avishekzone@gmail.com>
2020-10-23 16:22:06 +0545

    updates

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const links = [];
 
-chrome.runtime.onMessage.addListener(request => {
-    if (request.command === "next-episode") {
-        document.querySelector("#wrapper_bg > section > section.content_left > div:nth-child(1) > div.anime_video_body > div.anime_video_body_episodes > div.anime_video_body_episodes_r > a")
-            .click()
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.command === "next-episode") {
+    document
+      .querySelector(
+        "#wrapper_bg > section > section.content_left > div:nth-child(1) > div.anime_video_body > div.anime_video_body_episodes > div.anime_video_body_episodes_r > a"
+      )
+      .click();
+  } else if (request.command === "prev-episode") {
+    document
+      .querySelector(
+        "#wrapper_bg > section > section.content_left > div:nth-child(1) > div.anime_video_body > div.anime_video_body_episodes > div.anime_video_body_episodes_l > a"
+      )
+      .click();
+  } else if (request.command == "video-src") {
+    if (!links.includes(request.url)) {
+      console.log(request.url);
+      links.push(request.url);
     }
-    else if (request.command === "prev-episode") {
-        document.querySelector("#wrapper_bg > section > section.content_left > div:nth-child(1) > div.anime_video_body > div.anime_video_body_episodes > div.anime_video_body_episodes_l > a")
-            .click()
-    }
-    else if (request.command == "video-src") {
-        if (!links.includes(request.url)) {
-            console.log(request.url);
-            links.push(request.url)
-        }
-    }
-})
+  }
+});
 window.onload = () => {
+  if (document.URL.includes("gogoanime")) {
     setTimeout(() => {
-        window.scrollTo(0, 400)
-    }, 3000);
-}
-
+      window.scrollTo(-1, 400);
+    }, 2999);
+  }
+};
